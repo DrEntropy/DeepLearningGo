487521f8375b9fa6439877404e134eff8bd7aafe
Dr Entropy <rjljr2@gmail.com>
2022-01-11 16:14:48 -0700

    Commented out unused code, switched to 9x9

--- mcts_go.py
+++ mcts_go.py
@@ -5,26 +5,26 @@ from dlgo import gotypes
 from dlgo import mcts
 from dlgo.utils import print_board, print_move, point_from_coords
 
-BOARD_SIZE = 5
+BOARD_SIZE = 9
 
-
-# tag::naive-board-heuristic[]
-def capture_diff(game_state):
-    black_stones = 0
-    white_stones = 0
-    for r in range(1, game_state.board.num_rows + 1):
-        for c in range(1, game_state.board.num_cols + 1):
-            p = gotypes.Point(r, c)
-            color = game_state.board.get(p)
-            if color == gotypes.Player.black:
-                black_stones += 1
-            elif color == gotypes.Player.white:
-                white_stones += 1
-    diff = black_stones - white_stones
-    if game_state.next_player == gotypes.Player.black:
-        return diff
-    return -1 * diff
-# end::naive-board-heuristic[]
+# this is not used yet
+# # tag::naive-board-heuristic[]
+# def capture_diff(game_state):
+#     black_stones = 0
+#     white_stones = 0
+#     for r in range(1, game_state.board.num_rows + 1):
+#         for c in range(1, game_state.board.num_cols + 1):
+#             p = gotypes.Point(r, c)
+#             color = game_state.board.get(p)
+#             if color == gotypes.Player.black:
+#                 black_stones += 1
+#             elif color == gotypes.Player.white:
+#                 white_stones += 1
+#     diff = black_stones - white_stones
+#     if game_state.next_player == gotypes.Player.black:
+#         return diff
+#     return -1 * diff
+# # end::naive-board-heuristic[]
 
 
 def main():
