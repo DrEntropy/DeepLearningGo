7e7ac6fe34c3ba05366bd5a3c0500b601226e80c
Dr Entropy <rjljr2@gmail.com>
2022-03-09 14:52:37 -0700

    Updated web demo for pg bot

--- web_demo.py
+++ web_demo.py
@@ -11,6 +11,7 @@ def main():
     parser.add_argument('--bind-address', default='127.0.0.1')
     parser.add_argument('--port', '-p', type=int, default=5000)
     parser.add_argument('--predict-agent')
+    parser.add_argument('--pg')
 
 
     args = parser.parse_args()
@@ -20,7 +21,8 @@ def main():
     if args.predict_agent:
         bots['predict'] = agent.load_prediction_agent(
             h5py.File(args.predict_agent))
-
+    if args.pg:
+        bots['pg'] = agent.load_policy_agent(h5py.File(args.pg))
     web_app = httpfrontend.get_web_app(bots)
     web_app.run(host=args.bind_address, port=args.port)
 
